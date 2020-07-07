053a4467124ca80e2c5efc8aa6b24c43b43b5616
Ben Brown <bbrown@vsapartners.com>
2020-07-07 15:48:31 -0400

    fixes food prices random num generator to produce 0 as often as other nums

--- src/_js/modules/food-prices.js
+++ src/_js/modules/food-prices.js
@@ -91,7 +91,8 @@ function makePricesMove (data) {
 
 function validate (data, num, check) {
   // Choose a random number
-  randNum = Math.ceil(Math.random() * (num - 1))
+  randNum = Math.floor(Math.random() * num)
+  console.log(num, randNum)
 
   // Check to Make sure Factor Names are not the same.
   $.each(check, function (i, l) {
