6885689d616d297e1bfbbbe5ce40b80531b63d81
Jonathon Westbrook <jonathon.westbrook1@ibm.com>
2022-09-02 13:52:36 -0400

    copy update

--- src/_data/resource-cards.js
+++ src/_data/resource-cards.js
@@ -1,204 +1,204 @@
 module.exports = {
-  "learning.food-facts": {
-    eyebrow: "Learning module",
-    headline: "Facts About Food",
+  'learning.food-facts': {
+    eyebrow: 'Learning module',
+    headline: 'Facts About Food',
     copy:
-      "Take an interactive journey from the farm to the table with this educational resource.",
+      'Take an interactive journey from the farm to the table with this educational resource.',
     link:
-      "https://www.econessentialsinschool.com/files/InteractiveModules/food/#slide1-1",
-    image: "learning-food-fact.png",
-    icon: "link-external",
+      'https://www.econessentialsinschool.com/files/InteractiveModules/food/#slide1-1',
+    image: 'learning-food-fact.png',
+    icon: 'link-external'
   },
-  "learning.food-fuel-finance": {
-    eyebrow: "Learning modules",
-    headline: "Food, Fuel, and Finance",
+  'learning.food-fuel-finance': {
+    eyebrow: 'Learning modules',
+    headline: 'Food, Fuel, and Finance',
     copy:
-      "Engage with real-world examples of core economic principles through these interactive, educational resources.",
-    image: "food-fuel-finance.png",
-    link: "http://www.econessentialsinschool.com/interactive-modules",
-    icon: "link-external",
+      'Engage with real-world examples of core economic principles through these interactive, educational resources.',
+    image: 'food-fuel-finance.png',
+    link: 'http://www.econessentialsinschool.com/interactive-modules',
+    icon: 'link-external'
   },
-  "learning.foundations-finance": {
-    eyebrow: "Learning module",
-    headline: "Foundations of Finance",
+  'learning.foundations-finance': {
+    eyebrow: 'Learning module',
+    headline: 'Foundations of Finance',
     copy:
-      "See if you can navigate tricky financial situations – like dealing with student loans and buying a car – with this interactive resource.",
-    icon: "link-external",
+      'See if you can navigate tricky financial situations – like dealing with student loans and buying a car – with this interactive resource.',
+    icon: 'link-external',
     link:
-      "https://www.econessentialsinschool.com/files/InteractiveModules/foundations-of-finance/",
-    image: "learning-finance.jpg",
+      'https://www.econessentialsinschool.com/files/InteractiveModules/foundations-of-finance/',
+    image: 'learning-finance.jpg'
   },
-  "learning.fueling-future": {
-    eyebrow: "Learning module",
-    headline: "Fueling the Future",
+  'learning.fueling-future': {
+    eyebrow: 'Learning module',
+    headline: 'Fueling the Future',
     copy:
-      "See if you can predict future gas prices using our interactive, educational resource.",
-    icon: "link-external",
-    image: "learning-fueling-future.jpg",
+      'See if you can predict future gas prices using our interactive, educational resource.',
+    icon: 'link-external',
+    image: 'learning-fueling-future.jpg',
     link:
-      "https://www.econessentialsinschool.com/files/InteractiveModules/fuel/#package.json:-1",
+      'https://www.econessentialsinschool.com/files/InteractiveModules/fuel/#package.json:-1'
   },
-  "lesson.choice": {
-    eyebrow: "Economics lesson",
-    headline: "The Question of Choice",
+  'lesson.choice': {
+    eyebrow: 'Economics lesson',
+    headline: 'The Question of Choice',
     copy:
-      "Explore how individual decision-making can be influenced by peers, businesses, organizations, and others.",
+      'Explore how individual decision-making can be influenced by peers, businesses, organizations, and others.',
     link:
-      "https://www.econessentialsinschool.com/behavioral-econ-lessons#lesson-three",
-    image: "lesson-choice.jpg",
-    icon: "link-external",
+      'https://www.econessentialsinschool.com/behavioral-econ-lessons#lesson-three',
+    image: 'lesson-choice.jpg',
+    icon: 'link-external'
   },
-  "lesson.intro": {
-    eyebrow: "Economics lesson",
-    headline: "An Introduction to Behavioral Economics",
+  'lesson.intro': {
+    eyebrow: 'Economics lesson',
+    headline: 'An Introduction to Behavioral Economics',
     copy:
-      "Explore behavioral economic theory and the mysteries behind human decision-making.",
+      'Explore behavioral economic theory and the mysteries behind human decision-making.',
     link:
-      "https://www.econessentialsinschool.com/behavioral-econ-lessons#introduction",
-    image: "lesson-intro.jpg",
-    icon: "link-external",
+      'https://www.econessentialsinschool.com/behavioral-econ-lessons#introduction',
+    image: 'lesson-intro.jpg',
+    icon: 'link-external'
   },
-  "lesson.preference": {
-    eyebrow: "Economics lesson",
-    headline: "How We Are Affected by Preference",
+  'lesson.preference': {
+    eyebrow: 'Economics lesson',
+    headline: 'How We Are Affected by Preference',
     copy:
-      "Gain an understanding of how concepts like loss aversion and the endowment effect factor into the financial decision-making process.",
+      'Gain an understanding of how concepts like loss aversion and the endowment effect factor into the financial decision-making process.',
     link:
-      "https://www.econessentialsinschool.com/behavioral-econ-lessons#lesson-two",
-    image: "lesson-preference.jpg",
-    icon: "link-external",
+      'https://www.econessentialsinschool.com/behavioral-econ-lessons#lesson-two',
+    image: 'lesson-preference.jpg',
+    icon: 'link-external'
   },
-  "lesson.price": {
-    eyebrow: "Economics lesson",
-    headline: "How We Are Affected by Price",
+  'lesson.price': {
+    eyebrow: 'Economics lesson',
+    headline: 'How We Are Affected by Price',
     copy:
-      "Are people’s buying decisions dictated more by preference or by price? Learn how concepts like price anchoring can play a role.",
-    icon: "link-external",
-    image: "lesson-price.jpg",
+      'Are people’s buying decisions dictated more by preference or by price? Learn how concepts like price anchoring can play a role.',
+    icon: 'link-external',
+    image: 'lesson-price.jpg',
     link:
-      "https://www.econessentialsinschool.com/behavioral-econ-lessons#lesson-four",
+      'https://www.econessentialsinschool.com/behavioral-econ-lessons#lesson-four'
   },
-  "lesson.activity": {
-    eyebrow: "Classroom activity",
-    headline: "Changes in Supply and Demand",
+  'lesson.activity': {
+    eyebrow: 'Classroom activity',
+    headline: 'Changes in Supply and Demand',
     copy:
-      "Help students connect economics concepts to current events with a timely activity examining real-world applications of extreme changes in supply or demand.",
-    icon: "link-external",
-    image: "hot-topic-resource.jpg",
-    link: "https://www.econessentialsinschool.com/#hot-topic-activity",
+      'Help students connect economics concepts to current events with a timely activity examining real-world applications of extreme changes in supply or demand.',
+    icon: 'link-external',
+    image: 'hot-topic-resource.jpg',
+    link: 'https://www.econessentialsinschool.com/#hot-topic-activity'
   },
-  "pdf.book-of-risk": {
-    eyebrow: "Downloadable PDF",
-    headline: "The Book of Risk",
+  'pdf.book-of-risk': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'The Book of Risk',
     copy:
-      "Everything you want to know about risk, how it’s managed and why it matters.",
-    icon: "download",
-    link: "/assets/pdf/The_Book_of_Risk.pdf",
-    image: "pdf-book-of-risk.png",
-    "small-copy": "Contributed by CME Group",
+      'Everything you want to know about risk, how it’s managed and why it matters.',
+    icon: 'download',
+    link: '/assets/pdf/The_Book_of_Risk.pdf',
+    image: 'pdf-book-of-risk.png',
+    'small-copy': 'Contributed by CME Group'
   },
-  "pdf.clearing": {
-    eyebrow: "Downloadable PDF",
-    headline: "A Counterparty for Every Trade",
+  'pdf.clearing': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'A Counterparty for Every Trade',
     copy:
-      "Get a big-picture look at the clearing process of a large global exchange.",
-    icon: "download",
-    link: "/assets/pdf/A_Counterparty_for_Every_Trade.pdf",
-    image: "pdf-clearing.png",
+      'Get a big-picture look at the clearing process of a large global exchange.',
+    icon: 'download',
+    link: '/assets/pdf/A_Counterparty_for_Every_Trade.pdf',
+    image: 'pdf-clearing.png'
   },
-  "pdf.electronic-trade": {
-    eyebrow: "Downloadable PDF",
-    headline: "Market Protections of an Electronic Trade",
-    copy: "Get a comprehensive look at the many steps of clearing a trade.",
-    icon: "download",
-    link: "/assets/pdf/Market_Protections_of_an_Electronic_Trade.pdf",
-    image: "pdf-electronic-trade.png",
+  'pdf.electronic-trade': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'Market Protections of an Electronic Trade',
+    copy: 'Get a comprehensive look at the many steps of clearing a trade.',
+    icon: 'download',
+    link: '/assets/pdf/Market_Protections_of_an_Electronic_Trade.pdf',
+    image: 'pdf-electronic-trade.png'
   },
-  "pdf.market-abuse": {
-    eyebrow: "Downloadable PDF",
-    headline: "Preventing Market Abuse",
+  'pdf.market-abuse': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'Preventing Market Abuse',
     copy:
-      "See how trading is monitored and protected on a day-to-day basis at a global futures exchange.",
-    icon: "download",
-    link: "/assets/pdf/Preventing_Market_Abuse.pdf",
-    image: "pdf-market-abuse.png",
+      'See how trading is monitored and protected on a day-to-day basis at a global futures exchange.',
+    icon: 'download',
+    link: '/assets/pdf/Preventing_Market_Abuse.pdf',
+    image: 'pdf-market-abuse.png'
   },
-  "pdf.prices-beef": {
-    eyebrow: "Downloadable PDF",
-    headline: "The Facts Behind Beef Prices",
-    copy: "Take a closer look at the factors surrounding livestock prices.",
-    icon: "download",
-    link: "/assets/pdf/The_Facts_Behind_Beef_Prices.pdf",
-    image: "food-prices-beef.jpg",
+  'pdf.prices-beef': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'The Facts Behind Beef Prices',
+    copy: 'Take a closer look at the factors surrounding livestock prices.',
+    icon: 'download',
+    link: '/assets/pdf/The_Facts_Behind_Beef_Prices.pdf',
+    image: 'food-prices-beef.jpg'
   },
-  "pdf.prices-food": {
-    eyebrow: "Downloadable PDF",
-    headline: "The Facts Behind Food Prices",
-    copy: "Get more specifics about the prices of corn, wheat, soy and more.",
-    icon: "download",
-    link: "/assets/pdf/The_Facts_Behind_Food_Prices.pdf",
-    image: "food-prices-grain.jpg",
+  'pdf.prices-food': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'The Facts Behind Food Prices',
+    copy: 'Get more specifics about the prices of corn, wheat, soy and more.',
+    icon: 'download',
+    link: '/assets/pdf/The_Facts_Behind_Food_Prices.pdf',
+    image: 'food-prices-grain.jpg'
   },
-  "pdf.prices-oil": {
-    eyebrow: "Downloadable PDF",
-    headline: "The Facts Behind Oil Prices",
+  'pdf.prices-oil': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'The Facts Behind Oil Prices',
     copy:
-      "See a broad overview of the global factors that influence crude oil prices.",
-    icon: "download",
-    link: "/assets/pdf/The_Facts_Behind_Oil_Prices.pdf",
-    image: "pdf-oil-prices.jpg",
+      'See a broad overview of the global factors that influence crude oil prices.',
+    icon: 'download',
+    link: '/assets/pdf/The_Facts_Behind_Oil_Prices.pdf',
+    image: 'pdf-oil-prices.jpg'
   },
-  "pdf.prices-pork": {
-    eyebrow: "Downloadable PDF",
-    headline: "The Facts Behind Pork Prices",
-    copy: "Take a closer look at the factors surrounding pork prices.",
-    icon: "download",
-    link: "/assets/pdf/The_Facts_Behind_Pork_Prices.pdf",
-    image: "food-prices-pork.jpg",
+  'pdf.prices-pork': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'The Facts Behind Pork Prices',
+    copy: 'Take a closer look at the factors surrounding pork prices.',
+    icon: 'download',
+    link: '/assets/pdf/The_Facts_Behind_Pork_Prices.pdf',
+    image: 'food-prices-pork.jpg'
   },
-  "pdf.speculator": {
-    eyebrow: "Downloadable PDF",
-    headline: "Who is a Speculator?",
+  'pdf.speculator': {
+    eyebrow: 'Downloadable PDF',
+    headline: 'Who is a Speculator?',
     copy:
-      "Speculator activity often comes under scrutiny when the markets fluctuate.",
-    icon: "download",
-    image: "pdf-speculator.jpg",
-    link: "/assets/pdf/Who_is_a_Speculator.pdf",
+      'Speculator activity often comes under scrutiny when the markets fluctuate.',
+    icon: 'download',
+    image: 'pdf-speculator.jpg',
+    link: '/assets/pdf/Who_is_a_Speculator.pdf'
   },
-  "trading-simulator": {
-    eyebrow: "Trading simulator",
-    headline: "Already Know the Basics?",
+  'trading-simulator': {
+    eyebrow: 'Trading simulator',
+    headline: 'Already Know the Basics?',
     copy:
-      "Bypass the tutorial and start trading now using our practice simulator.",
-    icon: "link-external",
-    link: "http://futures-fundamentals-ndjmr3dnam.elasticbeanstalk.com",
-    image: "trading-simulator.png",
+      'Bypass the tutorial and start trading now using our practice simulator.',
+    icon: 'link-external',
+    link: 'http://futures-fundamentals-ndjmr3dnam.elasticbeanstalk.com',
+    image: 'trading-simulator.png'
   },
-  "video.algae": {
-    eyebrow: "Video series",
-    headline: "Algae as an Energy Source?",
+  'video.algae': {
+    eyebrow: 'Video series',
+    headline: 'Algae as an Energy Source?',
     copy:
-      "Learn how scientists in Hawaii use algae as an alternative fuel source. Continue learning with this mini documentary series by Seeker Stories.",
-    link: "http://www.econessentialsinschool.com/videos",
-    icon: "link-external",
-    image: "video-algae.jpg",
+      'Learn how scientists in Hawaii use algae as an alternative fuel source. Continue learning with this mini documentary series by Seeker Stories.',
+    link: 'http://www.econessentialsinschool.com/videos',
+    icon: 'link-external',
+    image: 'video-algae.jpg'
   },
-  "video.cuba": {
-    eyebrow: "Video series",
-    headline: "Owning a Restaurant in Post-Embargo Cuba",
+  'video.cuba': {
+    eyebrow: 'Video series',
+    headline: 'Owning a Restaurant in Post-Embargo Cuba',
     copy:
-      "Meet a restaurateur looking to capitalize on new opportunities and build her business. Continue learning with the three-part series by Seeker Stories.",
-    link: "http://www.econessentialsinschool.com/videos",
-    image: "video-cuba.jpg",
-    icon: "link-external",
+      'Meet a restaurateur looking to capitalize on new opportunities and build her business. Continue learning with the three-part series by Seeker Stories.',
+    link: 'http://www.econessentialsinschool.com/videos',
+    image: 'video-cuba.jpg',
+    icon: 'link-external'
   },
-  "video.farmers": {
-    eyebrow: "Video series",
-    headline: "New Generation of Farmers",
+  'video.farmers': {
+    eyebrow: 'Video series',
+    headline: 'New Generation of Farmers',
     copy:
-      "The future of the US food supply will soon be in the hands of millenials. Continue learning with this mini documentary series by Seeker Stories.",
-    link: "http://www.econessentialsinschool.com/videos",
-    image: "video-farmers.jpg",
-    icon: "link-external",
-  },
-};
+      'The future of the US food supply will soon be in the hands of millennials. Continue learning with this three-part mini documentary series.',
+    link: 'http://www.econessentialsinschool.com/videos',
+    image: 'video-farmers.jpg',
+    icon: 'link-external'
+  }
+}
